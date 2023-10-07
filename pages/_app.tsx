0908e02fc63230a49b4e59555feb881ddc581a7e
mingi1123 <min_gi1123@naver.com>
2023-10-07 22:26:23 +0900

    feat: Login Plz Page 1

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "../styles/global-style";
+import { theme } from "../styles/theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <Component {...pageProps} />
+      {/* <RefreshTokenHandler setInterval={setInterval} /> */}
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ styles/styled.d.ts
@@ -0,0 +1,10 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      blackOpacity70: string;
+      blackOpacity80: string;
+    };
+  }
+}
