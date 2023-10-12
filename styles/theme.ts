e3d5058bec7526cacc7229a65d5e5838d0bbe819
mingi1123 <min_gi1123@naver.com>
2023-10-13 01:53:14 +0900

    style: Refactor styles

--- styles/theme.ts
+++ styles/theme.ts
@@ -1,8 +1,19 @@
 import { DefaultTheme } from "styled-components";
 
+declare module "styled-components" {
+  export interface DefaultTheme {
+    colors: {
+      ivory: string;
+      brown: string;
+      brown05: string;
+    };
+  }
+}
+
 export const theme: DefaultTheme = {
   colors: {
-    blackOpacity70: "rgba(58, 58, 58, 0.7)",
-    blackOpacity80: "rgba(58, 58, 58, 0.8)",
+    ivory: "#f8f4e8",
+    brown: "#8a7b66",
+    brown05: "rgba(138, 123, 102, 0.5)",
   },
 };
