2f29be9fcde07926efbf4d94cab3be39c3ef95f7
yes-ee <yes0112@naver.com>
2023-11-06 16:37:40 +0900

    feat(mypage): add mode button color change when select

--- pages/mypage/components/matchHistory.tsx
+++ pages/mypage/components/matchHistory.tsx
@@ -4,9 +4,40 @@ import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import home from "../../../public/Icon/home.png";
+import axiosInstance from "../../../utils/axiosInstance";
 
 const MatchHistoryContainer = () => {
+  const { data: session } = useSession();
   const router = useRouter();
+
+  const [mode, setMode] = useState("rank");
+  const [matchHistory, setMatchHistory] = useState([]);
+
+  useEffect(() => {
+    getMatchHistory();
+  }, []);
+
+  useEffect(() => {
+    getMatchHistory();
+  }, [mode]);
+
+  const getMatchHistory = async () => {
+    try {
+      const userId = session?.user.user_id;
+      const response = await axiosInstance.get("/games/" + mode, {
+        params: {
+          id: userId,
+          offset: 0,
+        },
+      });
+      console.log("getMatchHistory() response");
+      console.log(response);
+      await setMatchHistory(response.data);
+    } catch (error) {
+      console.log("Error occured in getMatchHistory()");
+      console.log(error);
+    }
+  };
 
   const handleRouteLobby = async () => {
     router.push("/");
@@ -16,8 +47,16 @@ const MatchHistoryContainer = () => {
     <MatchHistoryFrame>
       <MatchHistoryHeader>
         <Mode>
-          <ModeButton> 일반 </ModeButton>
-          <ModeButton> 랭크 </ModeButton>
+          <ModeButton onClick={() => setMode("general")}>
+            <div className={`${mode === "general" ? "select" : "unselect"}`}>
+              일반
+            </div>
+          </ModeButton>
+          <ModeButton onClick={() => setMode("rank")}>
+            <div className={`${mode === "rank" ? "select" : "unselect"}`}>
+              랭크
+            </div>
+          </ModeButton>
         </Mode>
         <Button onClick={handleRouteLobby}>
           <InfoImage src={home} alt="home" />
@@ -34,10 +73,10 @@ const MatchHistoryFrame = styled.div`
   margin: 3%;
   width: 100%;
   height: 100%;
-  // background-color: skyblue;
   display: flex;
   flex-direction: column;
   align-items: center;
+  gap: 1%;
 `;
 
 const MatchHistoryHeader = styled.div`
@@ -50,29 +89,36 @@ const MatchHistoryHeader = styled.div`
 
 const Mode = styled.div`
   width: 80%;
-  height: 60%;
+  height: 100%;
   display: flex;
   flex-direction: row;
   justify-content: space-between;
   padding: 10%;
-  gap: 10%;
+  gap: 5%;
 `;
 
 const ModeButton = styled.button`
   width: 100%;
   height: 100%;
-  background-color: ${(props) => props.theme.colors.cream};
-  color: ${(props) => props.theme.colors.lightgold};
+  background-color: ${(props) => props.theme.colors.lightbrown};
   font-family: "GiantsLight";
   border-radius: 15px;
   border: none;
   cursor: pointer;
-  // background-color: ${(props) => props.theme.colors.lightgold};
-  // color: ${(props) => props.theme.colors.white};
+  .select {
+    border-radius: 15px;
+    background-color: ${(props) => props.theme.colors.lightgold};
+    color: ${(props) => props.theme.colors.white};
+  }
+  .unselect {
+    border-radius: 15px;
+    background-color: ${(props) => props.theme.colors.cream};
+    color: ${(props) => props.theme.colors.lightgold};
+  }
 `;
 
 const Button = styled.div`
-  height: 50%;
+  height: 30%;
   width: auto;
   padding: 0.5vh 1.5vh;
   background-color: ${(props) => props.theme.colors.beige};
@@ -84,9 +130,13 @@ const Button = styled.div`
 `;
 
 const InfoImage = styled(Image)`
-  height: 70%;
+  height: 100%;
   width: auto;
   cursor: pointer;
 `;
 
-const MatchHistoryBody = styled.div``;
+const MatchHistoryBody = styled.div`
+  height: 90%;
+  width: 100%;
+  background-color: ${(props) => props.theme.colors.beige};
+`;
