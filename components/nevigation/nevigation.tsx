bec637bd7f9001f7fd6ac214632dd3deb24310a5
mingi1123 <min_gi1123@naver.com>
2023-11-16 18:14:10 +0900

    feat(nevigation): friendslist render

--- components/nevigation/nevigation.tsx
+++ components/nevigation/nevigation.tsx
@@ -3,22 +3,67 @@ import Image from 'next/image';
 import react, { useEffect, useState } from 'react';
 import { useSession, signOut } from 'next-auth/react';
 import ProfileContainer from './myProfile';
+import SearchBarContainer from './searchBar';
 import { useSocket } from '../../utils/SocketProvider';
+import UserInfo from '../userInfo';
 
 interface friendData {
   friendId: number;
   freindNickName: string;
+  freindIntraName: string;
   freindProfile: string;
   status: string;
 }
 
 const Navigation = () => {
   const { data: session } = useSession();
+  const { socket } = useSocket();
+  const [friendsList, setFriendsList] = useState<friendData[]>([]);
+
+  useEffect(() => {
+    if (socket && friendsList.length === 0) {
+      socket.emitWithAck('friend-list').then((response) => {
+        console.log(response);
+        setFriendsList(response.body);
+      });
+    }
+  });
+
+  const handleAvatarPath = (avatar: string) => {
+    const apiUrl = 'http://localhost:8080/';
+    return apiUrl + avatar;
+  };
+
+  const handleStatus = (status: string) => {
+    if (status === 'OFFLINE') {
+      return '0';
+    } else if (status === 'ONLINE') {
+      return '1';
+    } else {
+      return '2';
+    }
+  };
 
   return (
     <Container>
       <ProfileContainer />
       <DivisionBar />
+      <SearchBarContainer />
+      <UserList>
+        {friendsList.map((friend, index) => (
+          <UserInfoFrame>
+            <UserInfo
+              key={index}
+              nickName={friend.freindNickName}
+              // intraName={friend.freindIntraName}
+              intraName="IntraName"
+              avatar={handleAvatarPath(friend.freindProfile)}
+              size={100}
+            />
+            <Status textColor={handleStatus(friend.status)}> ⦁&nbsp;{friend.status} </Status>
+          </UserInfoFrame>
+        ))}
+      </UserList>
       <>
         {session ? (
           <>
@@ -50,4 +95,43 @@ const DivisionBar = styled.div`
   width: 100%;
   height: 0.2vh;
   background-color: ${(props) => props.theme.colors.brown05};
+`;
+
+const UserList = styled.div`
+  width: 100%;
+  height: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+  overflow-y: auto;
+`;
+
+const UserInfoFrame = styled.div`
+  width: 100%;
+  height: auto;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 2% 5%;
+  box-sizing: border-box;
+`;
+
+const Status = styled.div<{ textColor: string }>`
+  display: flex;
+  flex-direction: row;
+  font-size: 0.8vw;
+  font-family: 'GiantsLight';
+  /* color: ${(props) => props.theme.colors.brown}; */
+  color: ${(props) => {
+    switch (props.textColor) {
+      case '0':
+        return props.theme.colors.red;
+      case '1':
+        return props.theme.colors.green;
+      default:
+        return props.theme.colors.brown;
+    }
+  }};
 `;
--- styles/theme.ts
+++ styles/theme.ts
@@ -1,6 +1,6 @@
-import { DefaultTheme } from "styled-components";
+import { DefaultTheme } from 'styled-components';
 
-declare module "styled-components" {
+declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
       ivory: string;
@@ -16,24 +16,28 @@ declare module "styled-components" {
       gold02: string;
       lightgold: string;
       indigo: string;
+      green: string;
+      red: string;
     };
   }
 }
 
 export const theme: DefaultTheme = {
   colors: {
-    ivory: "#f8f4e8",
-    brown: "#8a7b66",
-    brown05: "rgba(138, 123, 102, 0.5)",
-    pink: "#E2826A",
-    white: "#FFFFF7",
-    Emerald: "#04AFA6",
-    orange: "#F39801",
-    beige: "rgba(255, 251, 231, 0.5);",
-    cream: "#FFFBE7",
-    gold: "#BEA745",
-    gold02: "rgba(190, 167, 69, 0.2)",
-    lightgold: "#C1B175",
-    indigo: "#253B52",
+    ivory: '#f8f4e8',
+    brown: '#8a7b66',
+    brown05: 'rgba(138, 123, 102, 0.5)',
+    pink: '#E2826A',
+    white: '#FFFFF7',
+    Emerald: '#04AFA6',
+    orange: '#F39801',
+    beige: 'rgba(255, 251, 231, 0.5);',
+    cream: '#FFFBE7',
+    gold: '#BEA745',
+    gold02: 'rgba(190, 167, 69, 0.2)',
+    lightgold: '#C1B175',
+    indigo: '#253B52',
+    green: '#8AC68A',
+    red: '#FC736D',
   },
 };
